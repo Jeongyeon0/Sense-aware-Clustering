9d4acab4794fd437a5197d82ada704f745b334ed
Jeongyeon0 <162288329+Jeongyeon0@users.noreply.github.com>
2024-03-05 16:39:32 +0900

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,47 @@
+
+k=1
+result=SimCSE_svc_allow_synonym_result
+mode=complete
+
+fold_name=cos_05
+threshold=0.5
+
+for file_name in noun verb adj adv
+do
+    python3 allow_synonyms_hac4svc.py --vector_file SimCSE_vector/$file_name.vector --output $result/k_$k/$mode/$fold_name/$file_name.cls.result --nbit -1 --threshold $threshold --mode $mode --num_k 1
+done
+
+fold_name=cos_06
+threshold=0.6
+
+for file_name in noun verb adj adv
+do
+    python3 allow_synonyms_hac4svc.py --vector_file SimCSE_vector/$file_name.vector --output $result/k_$k/$mode/$fold_name/$file_name.cls.result --nbit -1 --threshold $threshold --mode $mode --num_k 1
+done
+
+
+fold_name=cos_07
+threshold=0.7
+
+for file_name in noun verb adj adv
+do
+    python3 allow_synonyms_hac4svc.py --vector_file SimCSE_vector/$file_name.vector --output $result/k_$k/$mode/$fold_name/$file_name.cls.result --nbit -1 --threshold $threshold --mode $mode --num_k 1
+done
+
+
+fold_name=cos_08
+threshold=0.8
+
+for file_name in noun verb adj adv
+do
+    python3 allow_synonyms_hac4svc.py --vector_file SimCSE_vector/$file_name.vector --output $result/k_$k/$mode/$fold_name/$file_name.cls.result --nbit -1 --threshold $threshold --mode $mode --num_k 1
+done
+
+
+fold_name=cos_09
+threshold=0.9
+
+for file_name in noun verb adj adv
+do
+    python3 allow_synonyms_hac4svc.py --vector_file SimCSE_vector/$file_name.vector --output $result/k_$k/$mode/$fold_name/$file_name.cls.result --nbit -1 --threshold $threshold --mode $mode --num_k 1
+done
